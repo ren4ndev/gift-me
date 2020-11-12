2748de6cdc4a482c8f899f91a164f404e4c89573
Renan Oliveira <oliveira.renan.rg@gmail.com>
2020-11-12 10:50:33 -0300

    Adicionando Bulma e FontAwesome ao projeto

--- gift-me-app/src/App.js
+++ gift-me-app/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
-
+import './bulma.min.css';
+import Logo from './Logo';
+import Login from './Login';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        GiftMe! init...
+        <Logo />
+        <Login />
       </div>
     );
   }
---
+++ gift-me-app/src/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Logo extends React.Component {
+  render() {
+    return (
+      <div className="Logo">
+        GiftMe!
+      </div>
+    );
+  }
+}
+
+export default Logo;
